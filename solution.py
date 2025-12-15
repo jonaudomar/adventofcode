775ff545be93c3f5c0a62b944c35fa81a31df43c
Jonathan D <jdelrue-web@proton.me>
2025-12-15 17:55:11 +0100

    solve day1

---
+++ solution.py
@@ -0,0 +1,22 @@
+def day1(input_file, start=50):
+    """
+    Solution of the day 1 of advent of code (2025)
+    """
+    dictionnary = []
+    with open(input_file, "r") as puzzle_input:
+        for line in puzzle_input:
+            dictionnary.append([line[0], int(line[1:].strip())])
+
+    for line in dictionnary:
+        if line[0] == 'L':
+            line[1] = -line[1]
+
+    counter = start
+    zero_counter = 0
+
+    for line in dictionnary:
+        counter = (counter + line[1]) % 100
+        if counter == 0:
+            zero_counter += 1
+
+    return zero_counter
